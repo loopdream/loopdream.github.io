4b6be3efe155655f1649ad0161d8504a2718f96c
Dominic Geargeoura <dom.geargeoura@rga.com>
2015-08-28 10:50:49 +0100

    config tweak

--- config.rb
+++ config.rb
@@ -4,11 +4,9 @@ configure :development do
   activate :livereload
 end
 
-
 set :css_dir, 'assets/css'
 set :js_dir, 'assets/js'
 set :images_dir, 'assets/images'
-
 
 # Build-specific configuration
 configure :build do
@@ -17,7 +15,7 @@ configure :build do
   activate :minify_html
 end
 
-
+# Push to github pages
 activate :deploy do |deploy|
   deploy.method = :git
   deploy.branch   = 'master'
